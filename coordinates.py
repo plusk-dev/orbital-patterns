4a9551ec799ac1da983a31c443c78779f4f2ca84
kirbyyourmom <rdiussss@gmail.com>
2022-01-07 15:27:18 +0000

    planents spawn over mouse cursor

--- coordinates.py
+++
@@ -1,8 +0,0 @@
-class Matrix():
-	def __init__(self, screen) -> None:
-		self.width = screen.get_width()
-		self.height = screen.get_height()
-		self.origin = ((self.width/2), (self.height/2))
-
-	def coords(self, x,y):
-		return (self.origin[0]+x, self.origin[1]-y)
